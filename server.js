8578761ebc7eda2d4b45cc85e5e1bb41df83ed4e
Vipin  Suresh <Vipin_Suresh@ad.infosys.com>
2017-07-03 11:07:29 +0530

    port frm process env

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express')
 var app = express();
-	app.set('port', 8080);
-	app.use(express.static(__dirname));
-	
+app.use(express.static(__dirname));	
 app.get('*',  function (req, res) {
   res.sendFile('index.html', { root: __dirname})
 })
-app.listen(8080);
+app.listen(process.env.PORT ||8080);
